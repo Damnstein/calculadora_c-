97088d3c6d45378078276f4ae8ccc7b64a7d577d
Damian <lopezdamianezequiel91@gmail.com>
2024-05-30 19:10:58 -0300

    Modificacion: variables mas descriptivas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-int sumar = 1;
-int restar = 2;
-int multiplicar = 3;
-int dividir = 4;
+int operacionSuma = 1;
+int operacionResta = 2;
+int operacionMultiplicar = 3;
+int operacionDividir = 4;
 int cero = 0;
 
 Console.WriteLine("Ingresa el numero de la operacion para seleccionarla: 1- Suma. 2- Resta. 3- Multiplicacion. 4- Division.");
@@ -11,18 +11,18 @@ int numUno = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingresa el segundo numero de la operacion.");
 int numDos = int.Parse(Console.ReadLine());
 
-if (eleccionNum == sumar){
+if (eleccionNum == operacionSuma){
     Console.WriteLine($"El resultado de {numUno} + {numDos}, es {numUno+numDos}.");
 }
-else if (eleccionNum == restar){
+else if (eleccionNum == operacionResta){
     Console.WriteLine($"El resultado de {numUno} - {numDos}, es {numUno-numDos}.");
 
 }
-else if (eleccionNum == multiplicar){
+else if (eleccionNum == operacionMultiplicar){
     Console.WriteLine($"El resultado de {numUno} * {numDos}, es {numUno*numDos}.");
 
 }
-else if (eleccionNum == dividir){
+else if (eleccionNum == operacionDividir){
     if(numUno == cero || numDos == cero){
         Console.Write($"Error: No es posible realizar divisiones por cero.");
     }
